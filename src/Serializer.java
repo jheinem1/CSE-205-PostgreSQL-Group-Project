f020afdb5eeb8da0442126edecd096342f005d37
exx <superjamrs323@gmail.com>
2021-04-23 14:33:32 -0700

    Added some comments and a deserialize method (doesn't work yet)

--- src/Serializer.java
+++ src/Serializer.java
@@ -6,6 +6,10 @@
 
 public class Serializer {
 
+    /**
+     * Serializes an iterable thingy of Person objects
+     * @return JSON object (it's a string)
+     */
     public static <T extends Iterable<Person>> String serialize(T array) {
         var out = new StringBuilder("[");
         for (var v : array)
@@ -13,10 +17,18 @@ public class Serializer {
         return out.append("]").toString();
     }
 
+    /**
+     * Serializes an individual Person object into a JSON object
+     * @return JSON object (it's a string)
+     */
     public static String serialize(Person person) {
         return String.format(
                 "{\"type\":\"Person\",\"firstName\":\"%s\",\"lastName\":\"%s\",\"address\":\"%s\",\"emailAddress\":\"%s\",\"position\":\"%s\"}",
                 person.getfName(), person.getlName(), person.getAddress(), person.getEmailAddress(),
                 person.getPosition());
+    }
+
+    public static Person deserialize(String json) {
+        throw new UnsupportedOperationException("Method not implemented yet");
     }
 }
