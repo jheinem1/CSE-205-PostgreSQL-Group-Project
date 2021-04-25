1c459ab63ec393c3e9542157f41c15e108180c6f
JJMcSquiggles <70779202+JJMcSquiggles@users.noreply.github.com>
2021-04-24 22:41:07 -0700

    (Nearly) Finished Cust, Emp, Manager
    Almost finished with the C, E, M classes.
    
    I added:
    Implentation of Inventory functions to interact with the inverntory for E and M
    Changed some syntax for C and shoppingCart
    
    Still waiting for:
    ShoppingCart.encode / serialize and ShoppingCard.decode / deserialize
    And INventory remove item and change price

---
+++ Manager.java
@@ -0,0 +1,76 @@
+import java.util.LinkedHashMap;
+
+public class Manager extends Person {
+
+	public Manager(String fName, String lName, String address, String emailAddress, String username,
+			String password) {//Needed info for Manager
+		super(fName, lName, address, emailAddress, "Manager", username, password);
+	}
+	
+	public void addCustomer(DbConnections base, String dbName, String fName, String lName, String address, String emailAddress, String position, String username,
+			String password) {//Same function used in Employee to Add a customer to the database by creating the nessasary items for Customer.java
+		
+		Customer cust = makeCustomer(fName, lName, address, emailAddress, username, password);
+		
+		LinkedHashMap<String, String> toInsert = new LinkedHashMap<String, String>();
+		toInsert.put("ID" , cust.getID() + "");
+		toInsert.put("Person" , cust.encode());
+		
+		base.insertCommand(base.getConnection(), DbConnections.generateInsertCommand(toInsert, dbName));
+	}
+	
+	private Customer makeCustomer(String fName, String lName, String address, String emailAddress, String username, String password) {//Same function used in Employee to Creates a customer based off needed information
+		Customer cust = new Customer(fName, lName, address, emailAddress, username, password);
+		return cust;
+	}
+	
+	public void removeCustomer(DbConnections base, String dbName, Customer cust) {//Same function used in Employee to remove a customer from the database
+		base.deleteCommand(base.getConnection(), DbConnections.generateDeleteCommand(dbName, "ID = " + cust.getID()));
+	}
+	
+	public void updateCustomer(DbConnections base, String dbName, Customer cust, String toSet, String value) {//Sane function used in Employee to update a customer's info within the database
+		base.updateCommand(base.getConnection(), DbConnections.generateUpdateCommand(dbName, toSet, value), dbName);
+	}
+	
+	public void addEmployee(DbConnections base, String dbName, String fName, String lName, String address, String emailAddress, String position, String username,
+			String password) {//Adds Employee to database based on needed information
+		
+		Employee emp = new Employee(fName, lName, address, emailAddress, username, password);
+		
+		LinkedHashMap<String, String> toInsert = new LinkedHashMap<String, String>();
+		toInsert.put("ID" , emp.getID() + "");
+		toInsert.put("Person" , emp.encode());
+		
+		base.insertCommand(base.getConnection(), DbConnections.generateInsertCommand(toInsert, dbName));
+	}
+	
+	private Employee makeEmployee(String fName, String lName, String address, String emailAddress, String username, String password) {//Creates a Employee based on needed information
+		Employee emp = new Employee(fName, lName, address, emailAddress, username, password);
+		return emp;
+	}
+	
+	public void removeEmployee(DbConnections base, String dbName, Employee emp) {//Removes a Employee from the database
+		base.deleteCommand(base.getConnection(), DbConnections.generateDeleteCommand(dbName, "ID = " + emp.getID()));
+	}
+	
+	public void updateEmployee(DbConnections base, String dbName, Employee emp, String toSet, String value) {//Used to update a Employee in the database
+		base.updateCommand(base.getConnection(), DbConnections.generateUpdateCommand(dbName, toSet, value), dbName);
+	}
+	
+	public void removePendingOrder(DbConnections base, String dbName, String orderID) {//Used to remove a pending order from the database
+		base.deleteCommand(base.getConnection(), DbConnections.generateDeleteCommand(dbName, "ID = " + orderID));
+	}
+	
+	public void addNewItemToInventory(Inventory invent, String itemName, int itemPrice, int itemQuantity) {
+		invent.createNewItem(itemName, itemPrice, itemQuantity);
+	}
+	
+	public void incInventoryQuantityInventoryByOne(Inventory invent, String itemName) {
+		invent.increaseQuantity(itemName);
+	}
+	
+	public void decInventoryQuantityInventory(Inventory invent, String itemName, int decAmount) {
+		invent.decreaseQuantity(itemName, decAmount);
+	}
+
+}
