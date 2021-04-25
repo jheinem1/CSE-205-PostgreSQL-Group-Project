ec91cfa3db9266783f836ae6ec46b9f1530e1352
JeremyandPop <81706402+JeremyandPop@users.noreply.github.com>
2021-04-24 21:20:50 -0700

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -3,12 +3,12 @@ import java.util.LinkedHashMap;
 public class Manager extends Person {
 
 	public Manager(String fName, String lName, String address, String emailAddress, String username,
-			String password) {
+			String password) {//Needed info for Manager
 		super(fName, lName, address, emailAddress, "Manager", username, password);
 	}
 	
 	public void addCustomer(DbConnections base, String dbName, String fName, String lName, String address, String emailAddress, String position, String username,
-			String password) {
+			String password) {//Same function used in Employee to Add a customer to the database by creating the nessasary items for Customer.java
 		
 		Customer cust = makeCustomer(fName, lName, address, emailAddress, username, password);
 		
@@ -19,21 +19,21 @@ public class Manager extends Person {
 		base.insertCommand(base.getConnection(), DbConnections.generateInsertCommand(toInsert, dbName));
 	}
 	
-	private Customer makeCustomer(String fName, String lName, String address, String emailAddress, String username, String password) {
+	private Customer makeCustomer(String fName, String lName, String address, String emailAddress, String username, String password) {//Same function used in Employee to Creates a customer based off needed information
 		Customer cust = new Customer(fName, lName, address, emailAddress, username, password);
 		return cust;
 	}
 	
-	public void removeCustomer(DbConnections base, String dbName, Customer cust) {
+	public void removeCustomer(DbConnections base, String dbName, Customer cust) {//Same function used in Employee to remove a customer from the database
 		base.deleteCommand(base.getConnection(), DbConnections.generateDeleteCommand(dbName, "ID = " + cust.getID()));
 	}
 	
-	public void updateCustomer(DbConnections base, String dbName, Customer cust, String toSet, String value) {
+	public void updateCustomer(DbConnections base, String dbName, Customer cust, String toSet, String value) {//Sane function used in Employee to update a customer's info within the database
 		base.updateCommand(base.getConnection(), DbConnections.generateUpdateCommand(dbName, toSet, value), dbName);
 	}
 	
 	public void addEmployee(DbConnections base, String dbName, String fName, String lName, String address, String emailAddress, String position, String username,
-			String password) {
+			String password) {//Adds Employee to database based on needed information
 		
 		Employee emp = new Employee(fName, lName, address, emailAddress, username, password);
 		
@@ -44,20 +44,20 @@ public class Manager extends Person {
 		base.insertCommand(base.getConnection(), DbConnections.generateInsertCommand(toInsert, dbName));
 	}
 	
-	private Employee makeEmployee(String fName, String lName, String address, String emailAddress, String username, String password) {
+	private Employee makeEmployee(String fName, String lName, String address, String emailAddress, String username, String password) {//Creates a Employee based on needed information
 		Employee emp = new Employee(fName, lName, address, emailAddress, username, password);
 		return emp;
 	}
 	
-	public void removeEmployee(DbConnections base, String dbName, Employee emp) {
+	public void removeEmployee(DbConnections base, String dbName, Employee emp) {//Removes a Employee from the database
 		base.deleteCommand(base.getConnection(), DbConnections.generateDeleteCommand(dbName, "ID = " + emp.getID()));
 	}
 	
-	public void updateEmployee(DbConnections base, String dbName, Employee emp, String toSet, String value) {
+	public void updateEmployee(DbConnections base, String dbName, Employee emp, String toSet, String value) {//Used to update a Employee in the database
 		base.updateCommand(base.getConnection(), DbConnections.generateUpdateCommand(dbName, toSet, value), dbName);
 	}
 	
-	public void removePendingOrder(DbConnections base, String dbName, String orderID) {
+	public void removePendingOrder(DbConnections base, String dbName, String orderID) {//Used to remove a pending order from the database
 		base.deleteCommand(base.getConnection(), DbConnections.generateDeleteCommand(dbName, "ID = " + orderID));
 	}
 	
